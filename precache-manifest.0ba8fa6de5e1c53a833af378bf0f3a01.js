2b66572e664dccf1f982de765e5bdedf7a546695
Roy Ben Haroosh <roy@Roys-MacBook-Pro-2.local>
2020-04-06 17:28:12 +0300

    Updates

---
+++ precache-manifest.0ba8fa6de5e1c53a833af378bf0f3a01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b878b9f634d7623eb20f63157438f59",
+    "url": "/israeli-whist/index.html"
+  },
+  {
+    "revision": "e0213ac1f9b828929ca3",
+    "url": "/israeli-whist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e01b26f50abb07c70ec9",
+    "url": "/israeli-whist/static/js/2.c5af540a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/israeli-whist/static/js/2.c5af540a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0213ac1f9b828929ca3",
+    "url": "/israeli-whist/static/js/main.d12cd3ea.chunk.js"
+  },
+  {
+    "revision": "dad1f79e49250f5ad251",
+    "url": "/israeli-whist/static/js/runtime-main.735c7ad0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/israeli-whist/static/media/logo.5d5d9eef.svg"
+  }
+]);
